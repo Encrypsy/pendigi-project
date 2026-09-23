88a318d8cae4a06edf950c8bc0df7072bcb9b06b
malya <malyamaritza07@gmail.com>
2026-09-24 06:30:38 +0700

    updated urls.py

--- pendigi/urls.py
+++ pendigi/urls.py
@@ -2,9 +2,11 @@ from django.urls import path, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='/articles/', permanent=False), name='home'),
     path('accounts/', include('accounts.urls')),
     path('articles/', include('articles.urls')),
     path('interactions/', include('interactions.urls')),
